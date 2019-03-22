064d92aec4edb6f9921e2de8b3fa139252dc6221
Rapha321 <ralpha19@hotmail.com>
2019-03-22 23:42:24 +0000

    called by https when the request is made

--- https_example.js
+++ https_example.js
@@ -2,3 +2,18 @@ var https = require('https');
 
 console.log('I did it!');
 
+var options = {
+  host: 'www.example.org',
+  path: '/'
+}
+
+// called by https when the request is made
+var callback = function() {
+  console.log('In response handler callback!')
+}
+
+console.log('Im about to make the request')
+
+https.request(options, callback).end();
+
+console.log("I have made the request")
